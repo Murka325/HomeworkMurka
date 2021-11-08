81d5cf89d7a76b1375ae482788215544bce80594
Murka325 <92324168+Murka325@users.noreply.github.com>
2021-11-08 23:23:49 +0300

    Add files via upload

---
+++ Метрополитен_1/Class2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metro1
+{
+    public class Station
+    {
+        string name;
+        string color;
+        Line line;
+        bool isWheelchairAccesible;
+        bool hasParkAndRide;
+        bool hasNearbyCableCar;
+        List<Station> transfers;
+        public Station(string name, string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public bool IsWheelchairAccesible()
+        {
+            return isWheelchairAccesible;
+        }
+        public bool HasParkAndRide()
+        {
+            return hasParkAndRide;
+        }
+        public bool HasNearbeCableCar()
+        {
+            return hasNearbyCableCar;
+        }
+        public string GetLineName()
+        {
+            return line.GetName();
+        }
+    }
+}
